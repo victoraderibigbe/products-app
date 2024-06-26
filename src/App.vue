2f576505bc6c988ae273bedb08505b571584a10f
Victor Aderibigbe <aderibigbetoluwani5@gmail.com>
2024-06-26 15:19:17 +0100

    Add loading animation

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,22 @@
 <template>
-  <HeaderComponent />
-  <div class="p-5">
-    <div v-if="products" class="flex items-center justify-evenly gap-3 flex-wrap">
+  <div v-if="products.length > 0">
+    <HeaderComponent />
+    <div class="flex flex-wrap items-center gap-3 p-5 justify-evenly">
       <ProductsCard :products="products" />
     </div>
-    <div v-else>
-      <div
-        class="flex items-center justify-center w-56 h-56 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700"
-      >
-        <div
-          class="px-3 py-1 text-xs font-medium leading-none text-center text-blue-800 bg-blue-200 rounded-full animate-pulse dark:bg-blue-900 dark:text-blue-200"
-        >
-          loading...
-        </div>
-      </div>
-    </div>
+  </div>
+  <div v-else class="flex items-center justify-center w-full h-screen">
+    <LoadingComponent />
   </div>
 </template>
 
 <script setup>
 import { useProductStore } from '@/stores/productStore'
 import { storeToRefs } from 'pinia'
-import { onMounted } from 'vue'
-import ProductsCard from './components/ProductsCard.vue'
+import { onMounted, ref } from 'vue'
+import ProductsCard from '@/components/ProductsCard.vue'
 import HeaderComponent from '@/components/HeaderComponent.vue'
+import LoadingComponent from '@/components/LoadingComponent.vue'
 
 const store = useProductStore()
 const { products } = storeToRefs(store)
