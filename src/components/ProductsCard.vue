7ee7f97f21279c671b390328563161756f5cbb3e
Victor Aderibigbe <aderibigbetoluwani5@gmail.com>
2024-06-26 16:11:08 +0100

    Add navbar

--- src/components/ProductsCard.vue
+++ src/components/ProductsCard.vue
@@ -1,15 +1,17 @@
 <template>
   <div
-    class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 h-[400px]"
+    class="w-full h-[300px] max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-100 transition"
     v-for="(product, index) in products"
     :key="index"
   >
-    <a href="#" class="flex items-center justify-center">
-      <img class="p-8 rounded-t-lg max-w-56" :src="product.image" alt="product image" />
+    <a href="#" class="">
+      <div class="flex items-center justify-center w-full mb-5 max-h-56">
+        <img class="object-cover h-56 p-8 rounded-t-lg" :src="product.image" alt="product image" />
+      </div>
     </a>
     <div class="px-5 pb-5">
       <a href="#">
-        <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+        <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white truncate ...">
           {{ product.title }}
         </h5>
       </a>
